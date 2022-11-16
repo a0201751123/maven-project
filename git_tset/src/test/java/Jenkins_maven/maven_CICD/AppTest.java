628dc1610eb9cafb07b4d268b024f63248e57983
a0201751123 <98209819+a0201751123@users.noreply.github.com>
2022-11-16 23:47:39 +0800

    maven_quickstart

---
+++ git_tset/src/test/java/Jenkins_maven/maven_CICD/AppTest.java
@@ -0,0 +1,20 @@
+package Jenkins_maven.maven_CICD;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
